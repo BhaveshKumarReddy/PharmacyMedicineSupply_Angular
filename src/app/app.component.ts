2a8e056482672791f2420e28a1fa01808fb95fc4
BhaveshKumarReddy <128818065+BhaveshKumarReddy@users.noreply.github.com>
2023-04-08 17:08:56 +0530

    Added Login Logout Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PharmacyMedicineSupply_Angular';
+  local:any;
+  constructor(){
+    this.local = localStorage;
+  }
 }
